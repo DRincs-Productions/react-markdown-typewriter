ba2b8d48ef30e1c48294ba7f218f8e74e10747c9
Black Ram <67595890+BlackRam-oss@users.noreply.github.com>
2026-04-05 15:41:11 +0200

    Update TypeScript to version 5.9.3 and modify tsup configuration to improve entry point handling.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-    entry: ["src/**/index.ts"],
+    target: "es2020",
+    entry: {
+        index: "src/index.ts",
+    },
     format: ["cjs", "esm"], // Build for commonJS and ESmodules
     dts: true, // Generate declaration file (.d.ts)
     treeshake: true,
-    splitting: false,
     clean: true,
     minify: true,
     bundle: true,
     skipNodeModulesBundle: false, // Skip bundling of node_modules
-    entryPoints: ["src/index.ts"],
     external: ["motion", "react", "react-dom", "react-markdown"],
     outExtension({ format }) {
         return {
